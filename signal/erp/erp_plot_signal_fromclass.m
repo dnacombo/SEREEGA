a8fb30ad960aa99bc6a13eaa4e12e7a8f6be959d
maximilien.chaumon <maximilien.chaumon@icm-institute.org>
2018-07-20 15:38:21 +0200

    erp_plot_signal_fromclass: add option to plot variability as a shaded area

--- signal/erp/erp_plot_signal_fromclass.m
+++ signal/erp/erp_plot_signal_fromclass.m
@@ -18,6 +18,8 @@
 %                default: 1
 %       baseonly - (0|1) whether or not to plot only the base signal,
 %                  without any deviations or sloping. default: 0
+%       shadeplot - (0|1) if ploting also deviations and sloping, plot as a
+%                   shaded area instead of other colored lines (default:0)
 %
 % Out:  
 %       h - handle of the generated figure
@@ -64,6 +66,7 @@ addRequired(p, 'epochs', @isstruct);
 
 addParameter(p, 'newfig', 1, @isnumeric);
 addParameter(p, 'baseonly', 0, @isnumeric);
+addParameter(p, 'shadeplot', 0, @isnumeric);
 
 parse(p, class, epochs, varargin{:})
 
@@ -71,6 +74,7 @@ class = p.Results.class;
 epochs = p.Results.epochs;
 newfig = p.Results.newfig;
 baseonly = p.Results.baseonly;
+shadeplot = p.Results.shadeplot;
 
 % getting time stamps
 x = 1:epochs.length/1000*epochs.srate;
@@ -83,53 +87,119 @@ if isfield(epochs, 'prestim')
 if newfig, h = figure('name', 'ERP signal', 'NumberTitle', 'off', 'ToolBar', 'none'); else, h = gcf; end
 hold on;
 
-% plotting the mean signal, no deviations applied
-plot(x, erp_generate_signal_fromclass(class, epochs, 'baseonly', 1), '-');
-
 if ~baseonly
-    % signal with maximum possible deviation (negative)
-    ax = gca;
+    if shadeplot
+        m = NaN(1,epochs.length/1000*epochs.srate);M = NaN(1,epochs.length/1000*epochs.srate);
+        nsim = 100;
+        for i = 1:nsim * epochs.n
+            tmp = erp_generate_signal_fromclass(class,epochs,'baseonly',0,'epochNumber',ceil(i/nsim));
+            m = min(m,tmp);
+            M = max(M,tmp);
+        end
+        shadebetween(x,M,m,[.8 .8 .8],'none',.5);
+    else
+        % signal with maximum possible deviation (negative)
+        ax = gca;
         colidx = ax.ColorOrderIndex;
         ax.ColorOrderIndex = colidx;
-    plot(x, erp_generate_signal( ...
+        plot(x, erp_generate_signal( ...
             class.peakLatency - class.peakLatencyDv - class.peakLatencyShift, ...
             class.peakWidth - class.peakWidthDv, ...
             class.peakAmplitude - class.peakAmplitudeDv, ...
             epochs.srate, epochs.length), ':');
-
-    % signal with maximum possible deviation (positive)
+        
+        % signal with maximum possible deviation (positive)
         ax.ColorOrderIndex = colidx;
-    plot(x, erp_generate_signal( ...
+        plot(x, erp_generate_signal( ...
             class.peakLatency + class.peakLatencyDv + class.peakLatencyShift, ...
             class.peakWidth + class.peakWidthDv, ...
             class.peakAmplitude + class.peakAmplitudeDv, ...
             epochs.srate, epochs.length), '--');
-
-    if any([class.peakLatencySlope, class.peakWidthSlope, class.peakAmplitudeSlope])
-       % additionally plotting the signal with maximum slope applied
+        
+        if any([class.peakLatencySlope, class.peakWidthSlope, class.peakAmplitudeSlope])
+            % additionally plotting the signal with maximum slope applied
             % mean
             plot(x, erp_generate_signal(...
-                    class.peakLatency + class.peakLatencySlope, ...
-                    class.peakWidth + class.peakWidthSlope, ...
-                    class.peakAmplitude + class.peakAmplitudeSlope, ...
-                    epochs.srate, epochs.length), '-');
-
+                class.peakLatency + class.peakLatencySlope, ...
+                class.peakWidth + class.peakWidthSlope, ...
+                class.peakAmplitude + class.peakAmplitudeSlope, ...
+                epochs.srate, epochs.length), '-');
+            
             % negative deviation
             ax.ColorOrderIndex = 2;
             plot(x, erp_generate_signal( ...
-                    class.peakLatency + class.peakLatencySlope - class.peakLatencyDv, ...
-                    class.peakWidth + class.peakWidthSlope - class.peakWidthDv, ...
-                    class.peakAmplitude + class.peakAmplitudeSlope - class.peakAmplitudeDv, ...
-                    epochs.srate, epochs.length), ':');
-
+                class.peakLatency + class.peakLatencySlope - class.peakLatencyDv, ...
+                class.peakWidth + class.peakWidthSlope - class.peakWidthDv, ...
+                class.peakAmplitude + class.peakAmplitudeSlope - class.peakAmplitudeDv, ...
+                epochs.srate, epochs.length), ':');
+            
             % positive deviation
             ax.ColorOrderIndex = 2;
             plot(x, erp_generate_signal( ...
-                    class.peakLatency + class.peakLatencySlope + class.peakLatencyDv, ...
-                    class.peakWidth + class.peakWidthSlope + class.peakWidthDv, ...
-                    class.peakAmplitude + class.peakAmplitudeSlope + class.peakAmplitudeDv, ...
-                    epochs.srate, epochs.length), '--');
+                class.peakLatency + class.peakLatencySlope + class.peakLatencyDv, ...
+                class.peakWidth + class.peakWidthSlope + class.peakWidthDv, ...
+                class.peakAmplitude + class.peakAmplitudeSlope + class.peakAmplitudeDv, ...
+                epochs.srate, epochs.length), '--');
+        end
     end
 end
 
+% plotting the mean signal, no deviations applied
+plot(x, erp_generate_signal_fromclass(class, epochs, 'baseonly', 1), '-');
+
+end
+
+function[fillhandle] = shadebetween(xpoints,upper,lower,color,edge,transparency)
+%USAGE: [fillhandle,msg]=jbfill(xpoints,upper,lower,color,edge,transparency)
+%This function will fill a region with a color between the two vectors provided
+%using the Matlab fill command.
+%
+%fillhandle is the returned handle to the filled region in the plot.
+%xpoints= The horizontal data points (ie frequencies). Note length(Upper)
+%         must equal Length(lower)and must equal length(xpoints)!
+%upper = the upper curve values (data can be less than lower)
+%lower = the lower curve values (data can be more than upper)
+%color = the color of the filled area 
+%edge  = the color around the edge of the filled area
+%transparency = value ranging from 1 for opaque to 0 for invisible for
+%       the filled color only.
+%
+%John A. Bockstege November 2006;
+%Example:
+%     a=rand(1,20);%Vector of random data
+%     b=a+2*rand(1,20);%2nd vector of data points;
+%     x=1:20;%horizontal vector
+%     [ph,msg]=jbfill(x,a,b,rand(1,3),rand(1,3),0,rand(1,1))
+%     grid on
+%     legend('Datr')
+if not(exist('transparency','var')) || isempty(transparency)
+    transparency=.5;
+end
+if not(exist('edge','var')) || isempty(edge)
+    edge = 'k';
+end
+if not(exist('color','var')) || isempty(color)
+    color = 'b';
+end
+if isvector(xpoints)
+    xpoints = xpoints(:);
+end
+if isvector(upper)
+    upper = upper(:);
+end
+if isvector(lower)
+    lower = lower(:);
+end
+filled=[upper;flipud(lower)];
+xpoints=[xpoints;flipud(xpoints)];
+nans = isnan(filled) | isnan(xpoints);
+filled(nans) = [];
+xpoints(nans) = [];
+
+fillhandle=fill(xpoints,filled,color,...
+    'EdgeColor',edge,'FaceAlpha',transparency,'EdgeAlpha',transparency);
+
+if nargout == 0
+    clear fillhandle
+end
 end
